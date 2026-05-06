4b8337e56d036ced3cc9a301237834165e6ace5a
Mohammed Ismail <mahammedismail1996@gmail.com>
2026-05-06 19:58:58 +0300

    Add Voting contract with proposal struct and newProposal function

---
+++ 4. Applied Solidity/Voting/Voting1.sol
@@ -0,0 +1,33 @@
+// proposal
+
+
+// PROBLEM:
+// Store proposals that contain:
+// - target address
+// - calldata (bytes)
+// - yes/no vote counts initialized to 0
+// Also store them in a public array.
+
+pragma solidity ^0.8.20;
+
+contract Voting {
+    struct Proposal {
+        address target;
+        bytes data;
+        uint yesCount;
+        uint noCount;
+    }
+
+    Proposal[] public proposals;
+
+    function newProposal(address target, bytes calldata data) external {
+        proposals.push(
+            Proposal({
+                target: target,
+                data: data,
+                yesCount: 0,
+                noCount: 0
+            })
+        );
+    }
+}
