c7c674c905948e7ad7631f72eb6a7afd51e8ee52
Gadisa Yusuf Aliyi <gadisa672@gmail.com>
2026-05-06 21:01:19 +0300

    Add inheritance examples (Hero, Mage, Warrior with override and super)

---
+++ 4. Applied Solidity/Inheritance/examples/HeroesInheritance1.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+/*
+PROBLEM:
+- There is a base contract Hero
+- Create two new contracts:
+    1. Mage
+    2. Warrior
+- Both should inherit ALL functionality from Hero
+*/
+
+import "./Hero.sol";
+
+contract Mage is Hero {
+
+}
+
+contract Warrior is Hero {
+
+}
